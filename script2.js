e8730b72cdb0b075204828cafc7a8dd7b43aa1f5
captainjakeyj <captainjakeyj@hotmail.com>
2025-10-05 13:57:26 +0100

    Fix division logic and input validation

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ const calculatorbutton = document.getElementById("calculatorbutton");
 calculatorbutton.onclick = function() {
     let number1 = parseFloat(prompt("Welcome to the calculator! Please enter the first number:"));
     let number2 = parseFloat(prompt("Please enter the second number:"))
-    if (isNaN(number1) || isNaN     (number2)) {
+    if (isNaN(number1) || isNaN(number2)) {
         alert("Invalid input. Please enter valid numbers.");
         return;
     }
@@ -12,7 +12,7 @@ calculatorbutton.onclick = function() {
     let product = `${number1} x ${number2} is ${number1 * number2}!`
     
     let quotient = ""
-    if (isNaN(quotientNumber)) {
+    if (quotientNumber !== 0 && number2 == 0) {
         quotient = "You cannot divide by 0! please rerun without dividing by 0 to see the division.";
     } else {
         quotient = `${number1} divided by ${number2} is ${number1 / number2}!`;
