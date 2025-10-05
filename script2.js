c8cdf87e24f106e8450d02ec6c97778f92b9df29
captainjakeyj <captainjakeyj@hotmail.com>
2025-10-05 13:39:45 +0100

    Update index and script2 logic

--- script2.js
+++ script2.js
@@ -8,14 +8,19 @@ calculatorbutton.onclick = function() {
         return;
     }
 
+    let quotientNumber = number1 / number2
     let product = `${number1} x ${number2} is ${number1 * number2}!`
-    let quotient = `${number1} divided by ${number2} is ${number1 / number2}!`;
-    let sum = `${number1} + ${number2} is ${number1 + number2}!`
-    let difference = `${number1} - ${number2} is ${number1 - number2}!`
+    
+    let quotient = ""
+    if (isNaN(quotientNumber)) {
+        quotient = "You cannot divide by 0! please rerun without dividing by 0 to see the division.";
+    } else {
+        quotient = `${number1} divided by ${number2} is ${number1 / number2}!`;
+    }
+    let sum = `${number1} + ${number2} is ${number1 + number2}!`;
+    let difference = `${number1} - ${number2} is ${number1 - number2}!`;
     alert(sum);
     alert(difference);
     alert(product);
-    if (number2 === 0 || number1 === 0) {
-        alert("Division by zero is undefined. Well, I guess if your eally want an answer.... It's 0. Happy?");
-    }
+    alert(quotient);
 }
